b6d96eb8a6ddd980c4f22ed1f23c42c98578177d
rhubarbhill <90863727+rhubarbhill@users.noreply.github.com>
2024-11-07 12:46:11 -0600

    Added boilerplate HTML/CSS/JS with some simple features

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to toggle visibility of additional information in the Analysis section
+function showMoreInfo() {
+    const moreInfo = document.getElementById("moreInfo");
+    if (moreInfo.style.display === "none") {
+        moreInfo.style.display = "block";
+    } else {
+        moreInfo.style.display = "none";
+    }
+}
